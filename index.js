da5935d86fb0b3061b2f53ed95a1dc87bceaf42b
Burak Orkmez <burakorkmezz@gmail.com>
2021-04-05 01:15:22 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+mybutton.addEventListener("click", () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+});
